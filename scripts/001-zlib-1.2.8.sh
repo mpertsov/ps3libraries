fbc202cea558ae8cc4fc719393b8796ac0f760e2
Andreas Öman <andreas@lonelycoder.com>
2013-05-21 16:20:25 +0200

    Update to zlib 1.2.8

--- scripts/001-zlib-1.2.8.sh
+++ scripts/001-zlib-1.2.8.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-# zlib-1.2.7.sh by Dan Peori (danpeori@oopo.net)
+# zlib-1.2.8.sh by Dan Peori (danpeori@oopo.net)
 
 ## Download the source code.
-wget --continue http://zlib.net/zlib-1.2.7.tar.gz || { exit 1; }
+wget --continue http://zlib.net/zlib-1.2.8.tar.gz || { exit 1; }
 
 ## Unpack the source code.
-rm -Rf zlib-1.2.7 && tar xfvz zlib-1.2.7.tar.gz && cd zlib-1.2.7 || { exit 1; }
+rm -Rf zlib-1.2.8 && tar xfvz zlib-1.2.8.tar.gz && cd zlib-1.2.8 || { exit 1; }
 
 ## Patch the source code.
-cat ../../patches/zlib-1.2.7-PPU.patch | patch -p1 || { exit 1; }
+cat ../../patches/zlib-1.2.8-PPU.patch | patch -p1 || { exit 1; }
 
 ## Configure the build.
 AR="ppu-ar" CC="ppu-gcc" RANLIB="ppu-ranlib" ./configure --prefix="$PS3DEV/host/ppu" --static || { exit 1; }
